ee82a0088f3d2989f38dbd40d5e7ad15effb997c
SachinPaytm <123359320+SachinPaytm@users.noreply.github.com>
2023-03-29 13:12:49 +0530

    Delete restful-web-services-user directory

--- restful-web-services-user/src/main/java/com/udemy/rest/webservices/restfulwebservices/jpa/UserRespository.java
+++
@@ -1,8 +0,0 @@
-package com.udemy.rest.webservices.restfulwebservices.jpa;
-
-import com.udemy.rest.webservices.restfulwebservices.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRespository extends JpaRepository<User,Integer> {
-
-}
